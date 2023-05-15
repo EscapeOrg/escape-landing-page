26ba5cd9fb1969c02b24f198a9684089f8e7d5f0
Alex Oh <alexoh0504@gmail.com>
2023-05-15 13:28:00 -0700

    Created basic navbar template

---
+++ initiate-react-project/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import "../styles/Navbar.css"
+
+
+function Navbar() {
+    return (
+        <ul id="navbar">
+            <li class="navbarItem" id="navbarLogo">Logo</li>
+            <li class="navbarItem">Test</li>
+            <li class="navbarItem">Test</li>
+            <li class="navbarItem">Test</li>
+            <li class="navbarItem">Test</li>
+        </ul>
+    );
+}
+
+export default Navbar;
